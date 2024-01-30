89c8658b38ca685fe6c3f3b90329fa7b132948f1
tarvialdur <tarvialdur@users.noreply.github.com>
2024-01-30 12:03:26 +0200

    Installing react bootstrap and testing it in login page

--- front-end/src/Login/index.js
+++ front-end/src/Login/index.js
@@ -1,5 +1,7 @@
 import React, { useState } from 'react';
 import { useLocalState } from '../util/useLocalStorage';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Button, Col, Container, Form, Row } from 'react-bootstrap';
 
 const Login = () => {
     const [username, setUsername] = useState("");
@@ -36,24 +38,73 @@ const Login = () => {
           });
     }
 
+
+
+
     return (
-        // react fragment <>
-        <> 
-        <div>
-            <label htmlFor="username">Username</label>
-            <input type="email" id="username" value={username} 
-            onChange={(e) => setUsername(e.target.value)}/>
-        </div>
-        <div>
-            <label htmlFor="password">Password</label>
-            <input type="password" id="password" value={password} 
-            onChange={(e) => setPassword(e.target.value)}/>
-        </div>
-        <div>
-            <button id="submit" type="button" onClick={() => sendLoginRequest()}>
+
+
+
+      <>
+      <Container className="mt-5" >
+
+
+        <Row className="justify-content-center">
+          <Col md="8" lg="6">
+        <Form.Group className="mb-3"  controlId="username">
+              <Form.Label className="fs-4">
+                Username
+              </Form.Label>
+              <Form.Control
+              type="email" 
+              size="lg"
+              placeholder=""
+              value={username} 
+              onChange={(e) => setUsername(e.target.value)}
+              />
+            </Form.Group>
+          </Col>
+        </Row>
+
+
+          <Row className="justify-content-center">
+            <Col md="8" lg="6">
+          <Form.Group className="mb-3" controlId="password">
+            <Form.Label className="fs-4">Password</Form.Label>
+            <Form.Control 
+            type="password"
+            size="lg"
+            placeholder=""
+            value={password} 
+            onChange={(e) => setPassword(e.target.value)}
+            />
+          </Form.Group>
+            </Col>
+          </Row>
+
+
+          <Row className="justify-content-center">
+            <Col md="8" lg="6" className="mt-2 d-flex flex-column gap-3 flex-md-row justify-content-md-between">
+              <Button 
+                id="submit" 
+                type="button" 
+                onClick={() => sendLoginRequest()}
+                size="lg"
+                >
                 Login
-            </button>
-        </div>
+              </Button>
+              <Button 
+                variant="secondary"
+                type="button" 
+                onClick={() =>{window.location.href ="/";
+              }}
+                size="lg"
+                >
+                Exit
+              </Button>
+          </Col>
+        </Row>
+      </Container>
         </>
     );
 };
