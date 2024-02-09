1b1b0dbaaab2c4ac435b6d84a335649bded48d35
tarvialdur <tarvialdur@users.noreply.github.com>
2024-02-09 17:43:46 +0200

    implementing comment time
    npm install dayjs
    wanted to implement comment time(time when was the comment created (Time from now)) and thought about taking in moment.js library, but i think day.js would be a bit smarter to do because it seems to be a minimized version of moment.js. If i need to use only 1% of the moment.js library, then the other 99% could slow down the UI maybe?

--- front-end/src/Comment/index.js
+++ front-end/src/Comment/index.js
@@ -1,23 +1,39 @@
 import React from 'react';
 import { useUser } from '../UserProvider';
 import { jwtDecode as jwt_decode } from "jwt-decode"; 
+import dayjs from 'dayjs';
+import relativeTime from "dayjs/plugin/relativeTime";
 
 const Comment = (props) => {
 
     const user = useUser();
     const decodedJwt = jwt_decode(user.jwt)
      
-    const { createdDate, id, creator, emitEditComment, text, emitDeleteComment } = props; 
+    const { 
+        createdDate, 
+        id, 
+        creator, 
+        emitEditComment, 
+        text, 
+        emitDeleteComment 
+    } = props; 
     
+
+    dayjs.extend(relativeTime);
+    const commentTime = dayjs(createdDate).fromNow();
+   
     console.log("decodedJWT", decodedJwt);
     console.log("creator", creator);
+
+
     return (
-            <div className="comment-bubble">
+    <>
+        <div className="comment-bubble">
                 <div className="d-flex gap-4" style={{ fontWeight: "bold" }}>
                     <div>{`${creator.name}`}</div>
                     {
                         decodedJwt.sub === creator.username ? (
-                        <>
+                            <>
                         <div 
                         onClick={() => {emitEditComment(id)}}  
                         style={{ cursor: "pointer", color:"darkblue"}}
@@ -26,17 +42,23 @@ const Comment = (props) => {
                     </div>
                     <div 
                         onClick={() => {emitDeleteComment(id)}} 
-                            style={{ cursor: "pointer", color:"darkblue"}}
-                            >
+                        style={{ cursor: "pointer", color:"darkblue"}}
+                        >
                             delete
                     </div>  
                     </> 
                     ) : (
-                    <></>
-                    )}
+                        <></>
+                        )}
                     </div>
                 <div>{text}</div>
             </div>
+            
+            <div style={{ marginTop: "-1em", marginLeft: "1.4em", fontSize:"15px"}}>
+            Posted: {commentTime}   
+            </div>
+            </>
+            
     );
 };
 
